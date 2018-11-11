47bb37d7fbd14352c5fa9c553dd7c5058e55acb6
Karina Kazintseva <karina2004200015@gmail.com>
2018-11-11 20:50:29 +0200

    kotiki done

---
+++ script.js
@@ -0,0 +1,85 @@
+var btn = document.getElementById("btn-link");
+console.log(btn);
+var modal = document.getElementById("modal");
+var close = document.getElementById("btn-close");
+console.log(modal);
+
+btn.addEventListener('click', function() {
+    modal.style.display = "block";
+});
+close.addEventListener('click', function() {
+    modal.style.display = "none";
+});
+
+// --------------------------------SLIDER
+jQuery(document).ready(function() {
+    function htmSlider() {
+
+        var slideWrap = jQuery('.slide-wrap');
+        var nextLink = jQuery('.next-slide');
+        var prevLink = jQuery('.prev-slide');
+
+        var playLink = jQuery('.auto');
+
+        var is_animate = false;
+
+        var slideWidth = jQuery('.slide-item').outerWidth();
+
+        var newLeftPos = slideWrap.position().left - slideWidth;
+
+        nextLink.click(function() {
+            if (!slideWrap.is(':animated')) {
+
+                slideWrap.animate({ left: newLeftPos }, 500, function() {
+                    slideWrap
+                        .find('.slide-item:first')
+                        .appendTo(slideWrap)
+                        .parent()
+                        .css({ 'left': 0 });
+                });
+
+            }
+        });
+
+        prevLink.click(function() {
+            if (!slideWrap.is(':animated')) {
+
+                slideWrap
+                    .css({ 'left': newLeftPos })
+                    .find('.slide-item:last')
+                    .prependTo(slideWrap)
+                    .parent()
+                    .animate({ left: 0 }, 500);
+
+            }
+        });
+
+        function autoplay() {
+            if (!is_animate) {
+                is_animate = true;
+                slideWrap.animate({ left: newLeftPos }, 500, function() {
+                    slideWrap
+                        .find('.slide-item:first')
+                        .appendTo(slideWrap)
+                        .parent()
+                        .css({ 'left': 0 });
+                    is_animate = false;
+                });
+            }
+        }
+
+        playLink.click(function() {
+            if (playLink.hasClass('play')) {
+                playLink.removeClass('play').addClass('pause');
+                jQuery('.navy').addClass('disable');
+                timer = setInterval(autoplay, 1000);
+            } else {
+                playLink.removeClass('pause').addClass('play');
+                jQuery('.navy').removeClass('disable');
+                clearInterval(timer);
+            }
+        });
+
+    }
+    htmSlider();
+});
